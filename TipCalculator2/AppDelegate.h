74cfd3dc78e0874b56b150d0c5e192e907e474dc
Benson Huynh <quoc@shaw.ca>
2016-01-22 11:57:55 -0800

    Initial Commit

---
+++ TipCalculator2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator2
+//
+//  Created by Benson Huynh on 2016-01-22.
+//  Copyright © 2016 Benson Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator2
+//
+//  Created by Benson Huynh on 2016-01-22.
+//  Copyright © 2016 Benson Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
